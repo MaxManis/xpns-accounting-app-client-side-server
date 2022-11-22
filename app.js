4d9da760cc39fdba5ee993edbce27f9445f47895
Max Bovtun <maxbovtun@gmail.com>
2022-11-22 15:36:13 +0200

    client side server first commit

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ const staticFolder = path.join(__dirname, 'build', 'static');
 app.use('/static', express.static(staticFolder));
 
 app.get('/', (req, res) => {
-    console.log(req.params)
-
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.get('*', (req, res) => {
+    res.statusCode = 404;
+    res.send('<h1>404: NOT FOUND</h1>');
+})
 
 app.listen(PORT);
